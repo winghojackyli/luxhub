ebbf30771d7e4a394bd4cf4a265bd83aa9326506
Jacky Li <jackylihk123@gmail.com>
2023-08-07 18:13:35 -0700

    corrected base url

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://luxhub-api.onrender.com/";
+const BASE_URL = "https://luxhub-api.onrender.com/api/";
 
 const currentUser = JSON.parse(
   localStorage.getItem("persist:root")
