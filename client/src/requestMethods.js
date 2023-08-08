80df982f0316bf81e52ca3d24199dbb9858bec36
Jacky Li <jackylihk123@gmail.com>
2023-08-08 12:37:32 -0700

    removed unused data in data.js

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -8,11 +8,18 @@ const currentUser = JSON.parse(
 
 const TOKEN = currentUser && JSON.parse(currentUser)?.accessToken;
 
+const getToken = () => {
+  const currentUser = JSON.parse(
+    localStorage.getItem("persist:root")
+  )?.currentUser;
+  return currentUser && JSON.parse(currentUser)?.accessToken;
+};
+
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  headers: { token: TOKEN },
+  headers: { token: TOKEN || getToken() },
 });
