09a03eac2bab5d2d10f32505fd1ed2df50aeeaf4
Jacky Li <jackylihk123@gmail.com>
2023-08-07 17:29:58 -0700

    added disableReactDevTools

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
